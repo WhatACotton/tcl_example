20d5e7fa8a4b6bf07e67aef6df5a13957894dbfe
whatacotton <whatacotton@gmail.com>
2025-05-07 22:03:00 +0900

    delete unused line

--- sim.tcl
+++ sim.tcl
@@ -1,12 +1,7 @@
 set top_module_name top
-set outputDir ./synth_tmp
-file mkdir $outputDir
 set_property top tb [current_fileset -simset]
-set_property top top [current_fileset]
 
-set_part xc7z020clg400-1
 read_verilog ./src/top.v
 read_verilog ./src/tb.v
-read_xdc ./src/pin.xdc
 
 export_simulation -force -simulator xsim
